0a3473bb8e2e1597a3c533eeddb0c61f6809d52c
theabhijeetsing <inabhijeet241107@gmail.com>
2025-02-14 22:23:16 +0530

    Java Script

---
+++ loops.js
@@ -0,0 +1,32 @@
+// LOOPs
+
+// Types of Loops-
+// >For Loop-If you know how many times you have loop.
+// >While Loop-If you don't know how many times youhave to loop.
+// >Do-while loop-same as while loop but first runs the code and then checks the condition.the code must runs one time.
+
+
+for (let i = 1; i <=10; i++) {
+    console.log("Hello" + i)
+    
+}
+
+
+
+
+while (ip != house) {
+    ip = ip + 1;
+    console.log("Step-taken: " + ip);
+}
+
+
+
+
+
+let ip = 0;
+let house = 20;
+
+do {
+    ip = ip + 1;
+ console.log("Step-taken: " + ip);
+} while (ip = house);
---
+++ operators.js
@@ -0,0 +1,20 @@
+// LOgical Operators In Java Script
+
+// AND operator- all the conditions should be true (  &&  )
+// OR operator- atleast one condition should be true (   ||  )
+// NOT operator- makes false > true and true> false  (  ! )
+
+const age=25;
+const gender="male";
+
+if (age=>18 && gender=="male") {
+    console.log("He is an adult")
+}
+
+
+
+
+// const number=10;
+// if (!(number%2==0)) {
+//     console.log("Even")
+// }
