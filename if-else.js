e27a33c4e4008c8c198da62707f589f63f0a6e4f
theabhijeetsingh <inabhijeet241107@gmail.com>
2025-02-16 12:15:00 +0530

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,45 @@
+// if-else statement
+
+{
+let age=25;
+
+if (age>=18) {
+    console.log("You are eligible to vote.")
+}
+}
+
+
+
+{
+let gender = "male"
+
+if (gender = "male") {
+console.log("You are a guy.")
+}
+}
+
+
+// SWITCH STATEMENT
+
+let operation ="+";
+let a = 5;
+let b = 2;
+
+
+switch (operation) {
+    case "+":
+        console.log(a+b)
+        break;
+
+    case "-":
+        console.log(a-b)
+            break;
+
+    case "*":
+         console.log(a*b)
+                break;
+            
+    default:
+         console.log(Error)
+        break;
+}
