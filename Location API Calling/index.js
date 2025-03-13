4014d6939321cce04291ff8afeb39ad0eb274a7d
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-03-13 21:03:47 +0530

    JavaScript

--- Location API Calling/index.js
+++ Location API Calling/index.js
@@ -1,1 +1,14 @@
-const button = document.getElementById("search-Button")
+
+const locationButton = document.getElementById("searchButton");
+
+function gotLocation(position) {
+    console.log("Got Location", position);
+}
+
+function notLocation(error) {
+    console.error("Error getting location:", error.message);
+}
+
+locationButton.addEventListener("click", async () => {
+        navigator.geolocation.getCurrentPosition(gotLocation, notLocation); 
+});
